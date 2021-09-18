f44d8e4764ac98d174387e95649195aa969a194b
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-18 13:48:07 -0400

    Correct syntax for factory to migrate

--- spec/factories/portfolios.rb
+++ spec/factories/portfolios.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     region { Faker::Types.rb_string }
     street_address { Faker::Address.street_address }
     city { Faker::Address.city }
-    state { Faker::Address. }
+    state { Faker::Address.state }
     zip_code { Faker::Address.zip_code }
     total_sqft { Faker::Number.decimal(l_digits: 8, r_digits: 1) }
     entity { nil }
