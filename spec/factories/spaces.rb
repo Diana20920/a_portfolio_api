c332b4abb0c3ea4fd0306679d4960f467d781ffe
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-19 20:52:17 -0400

    Replace faker boolean with a default value of true

--- spec/factories/spaces.rb
+++ spec/factories/spaces.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :space do
     category { Faker::TvShows::Friends.location }
-    vacant { Faker::Boolean.boolean }
+    vacant { true }
     square_footage { Faker::Number.decimal(l_digits: 3, r_digits: 1) }
     dollar_per_sqft { Faker::Commerce.price(range: 10..40.0, as_string: true) }
     site { nil }
