e4853bd9f9a53d40baaca4f0dd8317a2b1714a46
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-20 03:04:33 -0400

    Pass updated portfolio spec

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,29 @@
 class Portfolio < ApplicationRecord
   belongs_to :entity
   has_many :sites
+  has_many :spaces, through: :sites
 
   validates :region, presence: true
+
+  def sqft_statistics(id)
+    {
+      portfolio_id: self.id,
+      region: self.region,
+      total_spaces: self.spaces.count,
+      total_sqft: self.portfolio_total_sqft(id),
+      average_sqft: self.portfolio_average_sqft(id).round(2)
+    }
+  end
+
+  def portfolio_total_sqft(id)
+    portfolio = Portfolio.find(id)
+    portfolio.spaces.sum(:square_footage)
+  end
+
+  def portfolio_average_sqft(id)
+    portfolio = Portfolio.find(id)
+    total_spaces = portfolio.spaces.sum(:square_footage)
+    average = total_spaces / (portfolio.spaces.count)
+    average.round(2)
+  end
 end
