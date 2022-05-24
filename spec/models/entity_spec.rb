7e8bb886a5187c7a8c7569f61402b77049da57cd
Diana Buffone <diana.pamelaet@gmail.com>
2022-05-24 11:58:43 -0400

    Add portfolio name uniqueness validation

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Entity do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :address }
+    it { should validate_uniqueness_of :name }
   end
 end
